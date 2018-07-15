4d015dade7c9a4513f3706f39cbe6b979643b2cb
Giovanni Panasiti <giova.panasiti@gmail.com>
2018-07-15 17:06:26 +0200

    add dispose methods

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,14 +1,25 @@
 import 'dart:async';
+import 'package:login_bloc/src/blocs/vaidators.dart';
 
-class Bloc {
+class Bloc extends Validators {
   final _emailController = StreamController<String>();
   final _passwordController = StreamController<String>();
 
 //  add data to stream
   Function(String) get changeEmail => _emailController.sink.add;
+
   Function(String) get changePassword => _passwordController.sink.add;
 
 //  retrieve data from the stream
-  Stream<String> get emailStream => _emailController.stream;
-  Stream<String> get passwordStream => _passwordController.stream;
+  Stream<String> get emailStream =>
+      _emailController.stream.transform(validateEmail);
+
+  Stream<String> get passwordStream =>
+      _passwordController.stream.transform(validatePassword);
+
+
+  dispose() { // we can clean up our controller. Dispose is just a name convention
+    _emailController.close();
+    _passwordController.close();
+  }
 }
---
+++ lib/src/blocs/vaidators.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+class Validators {
+  final validateEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains('@')) {
+      sink.add(email);
+    } else {
+      sink.addError('Enter a Valid email');
+    }
+  });
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length > 3) {
+      sink.add(password);
+    } else {
+      sink.addError('Enter a Valid email');
+    }
+  });
+}
